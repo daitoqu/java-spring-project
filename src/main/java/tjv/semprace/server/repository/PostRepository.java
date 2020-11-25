45454edf38ad0d88d45c41d530183be9aa7858ae
Ivan Alekhin <alekhiva@fit.cvut.cz>
2020-11-25 17:56:26 +0100

    Repo fix

--- src/main/java/tjv/semprace/server/repository/PostRepository.java
+++ src/main/java/tjv/semprace/server/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package tjv.semprace.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tjv.semprace.server.entity.Post;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 }
--- src/main/java/tjv/semprace/server/repository/UserRepository.java
+++ src/main/java/tjv/semprace/server/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package tjv.semprace.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tjv.semprace.server.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
